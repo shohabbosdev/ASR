819eab56d77c5d8874272f13f4ec55c95c1cbfc5
shohabbosdev <ushohabbos@gmail.com>
2025-03-04 13:05:45 +0500

    tts qo'shildi

--- app.py
+++ app.py
@@ -42,46 +42,56 @@ def text_to_audio(text):
         st.error(f"xatolik: {ex}")  
         return None  
 
-st.markdown("# 🌐:rainbow[Audiodan matnga o'tkazish]")  
-# tab1 = st.tabs(["🔈 STT (Audiodan matnga o'tkazish)"])  
+st.markdown("# 🌐:rainbow[Audio va matn bilan ishlash]")  
+tab1, tab2 = st.tabs(["🔈 STT (Audiodan matnga o'tkazish)", "📝 TTS (Matngdan audioga o'tkazish)"])  
 
- 
-faollatgich = st.toggle(":rainbow[Audio yuklaymizmi?]")  
+with tab1:
+    faollatgich = st.toggle(":rainbow[Audio yuklaymizmi?]")  
 
-if faollatgich:  
-    audio_file = st.file_uploader("Audio fayl yuklang", type=["ogg", "mp3", "wav"])  
-else:  
-    audio_file = st.audio_input(":rainbow[Ovoz yozishni boshlang]")  
-
-try:  
-    if audio_file is not None:  
-        boshlash = time()  
-        result = stt(audio_file.read())['text']  
-        st.text_area("Natijamiz", value=result, disabled=True)  
-        st.toast("So'zlovchini aniqlash muvaffaqiyatli amalga oshirildi!", icon='🎉')  
-        tugash = time() - boshlash  
-        st.write(f"Dasturning ishlash vaqti: {round(tugash, 2)} sekund")  
-        st.audio(audio_file)  
-
-        document = write_word(result)  
-
-        byte_io = BytesIO()  
-        document.save(byte_io)  
-        byte_io.seek(0)    
-
-        st.download_button(  
-            label="Word faylini yuklash",  
-            data=byte_io,  
-            file_name=f"{audio_file.name}.docx",  
-            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document", 
-            icon='💾' 
-        )  
-    elif faollatgich:  
-        st.info("Marhamat qilib audio yuklang")  
+    if faollatgich:  
+        audio_file = st.file_uploader("Audio fayl yuklang", type=["ogg", "mp3", "wav"])  
     else:  
-        st.info("Mikrofon tugmasini bosib brauzeringizda unga ruxsat bering hamda ovozingni yozing")  
-except Exception as e:  
-    st.error(f"Xatolik paydo bo'ldi xatolik nomi: {str(e)}")  
+        audio_file = st.audio_input(":rainbow[Ovoz yozishni boshlang]")  
 
+    try:  
+        if audio_file is not None:  
+            boshlash = time()  
+            result = stt(audio_file.read())['text']  
+            st.text_area("Natijamiz", value=result, disabled=True)  
+            st.toast("So'zlovchini aniqlash muvaffaqiyatli amalga oshirildi!", icon='🎉')  
+            tugash = time() - boshlash  
+            st.write(f"Dasturning ishlash vaqti: {round(tugash, 2)} sekund")  
+            st.audio(audio_file)  
+
+            document = write_word(result)  
+
+            byte_io = BytesIO()  
+            document.save(byte_io)  
+            byte_io.seek(0)    
+
+            st.download_button(  
+                label="Word faylini yuklash",  
+                data=byte_io,  
+                file_name=f"{audio_file.name}.docx",  
+                mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document", 
+                icon='💾' 
+            )  
+        elif faollatgich:  
+            st.info("Marhamat qilib audio yuklang")  
+        else:  
+            st.info("Mikrofon tugmasini bosib brauzeringizda unga ruxsat bering hamda ovozingni yozing")  
+    except Exception as e:  
+        st.error(f"Xatolik paydo bo'ldi xatolik nomi: {str(e)}")  
+with tab2:  
+    matn = st.chat_input("Matn kiriting")  
+    if matn:  
+        with st.spinner("🔈 Audio yaratilmoqda iltimos ozgina vaqt kuting..."):  
+            st.text_area('✍️ Natija:', value=matn.capitalize(), disabled=True)  
+            audio_data = text_to_audio(matn)  
+            
+            if audio_data:  
+                st.audio(audio_data, format='audio/ogg', autoplay=True)  
+            else:  
+                st.error("Audio yaratishda xatolik yuz berdi.")
 st.divider()  
 st.markdown("[Bog'lanish uchun](https://t.me/shohabbosdev)")
